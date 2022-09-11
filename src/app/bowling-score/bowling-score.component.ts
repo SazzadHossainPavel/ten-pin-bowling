9a7de518767591308c2200a0be00c716ae8243a4
SazzadHossainPavel <sazzadhossain.pavel.3@gmail.com>
2022-09-11 19:53:41 +0600

    explanation added on code

--- src/app/bowling-score/bowling-score.component.ts
+++ src/app/bowling-score/bowling-score.component.ts
@@ -22,6 +22,7 @@ export class BowlingScoreComponent {
     this.initializeGame();
   }
 
+  // adds random generated knocked pins from the available options and continue the game
   addRandomPins() {
     this.isSpecificPins = false;
     this.isRandomPins = true;
@@ -30,6 +31,7 @@ export class BowlingScoreComponent {
     this.startGame(n);
   }
 
+  // adds specific knocked pins and continue the game
   addSpecificPins(n: number) {
     this.isSpecificPins = true;
     this.isRandomPins = false;
@@ -38,6 +40,7 @@ export class BowlingScoreComponent {
     this.checkAvailablePins(n);
   }
 
+  // clears previous score and initializes the game
   clearScore() {
     this.allPins = [];
     this.knockedPins = [];
@@ -52,6 +55,7 @@ export class BowlingScoreComponent {
     this.initializeGame();
   }
 
+  // initializes available pins and knocked pins array
   private initializeGame() {
     for (let i = 0; i < this.noOfFrames; i++) {
       this.allPins.push({ isAvailable: true, value: i });
@@ -61,12 +65,14 @@ export class BowlingScoreComponent {
     this.allPins.push({ isAvailable: true, value: this.noOfFrames });
   }
 
+  // generates random roll from the available options
   private randomRoll(firstRoll?: number) {
     return firstRoll
       ? Math.ceil(Math.random() * (11 - firstRoll)) - 1
       : Math.ceil(Math.random() * 11) - 1;
   }
 
+  // returns current roll index
   private getRollIndex(n: number): number {
     if (
       this.currentFrame !== -1 &&
@@ -78,6 +84,7 @@ export class BowlingScoreComponent {
     }
   }
 
+  // returns current roll score
   private getRandomRollScore() {
     let currentScore = 0;
 
@@ -85,8 +92,10 @@ export class BowlingScoreComponent {
       this.currentFrame === -1 ||
       this.knockedPins[this.currentFrame].rolls[0] === ''
     ) {
+      // when first roll value is not initialized, generates random number from 0 to 10
       currentScore = this.randomRoll();
     } else {
+      // when first roll value is initialized, generates random number from 0 to first roll
       currentScore = this.randomRoll(
         Number(this.knockedPins[this.currentFrame].rolls[0])
       );
@@ -95,9 +104,11 @@ export class BowlingScoreComponent {
     return currentScore;
   }
 
+  // tracks whether the game is over or not
   private checkGameOver() {
     if (this.currentFrame === 9) {
       if (
+        // game is over when 10th frame first roll is a strike and completes all the 3 rolls
         Number(this.knockedPins[9].rolls[0]) === 10 &&
         this.knockedPins[9].rolls[1] !== '' &&
         this.knockedPins[9].rolls[2] !== ''
@@ -105,6 +116,7 @@ export class BowlingScoreComponent {
         this.isGameOver = true;
         return;
       } else if (
+        // or, game is over when 10th frame has a spare and completes all the 3 rolls
         Number(this.knockedPins[9].rolls[0]) !== 10 &&
         Number(this.knockedPins[9].rolls[0]) +
           Number(this.knockedPins[9].rolls[1]) ===
@@ -114,6 +126,7 @@ export class BowlingScoreComponent {
         this.isGameOver = true;
         return;
       } else if (
+        // otherwise, game is over when 10th frame completes all the 2 rolls
         Number(this.knockedPins[9].rolls[0]) !== 10 &&
         Number(this.knockedPins[9].rolls[0]) +
           Number(this.knockedPins[9].rolls[1]) !==
@@ -126,20 +139,26 @@ export class BowlingScoreComponent {
     }
   }
 
+  // calculates cumulative score and total score
   private calculateScore() {
     const secondLastFrame = this.currentFrame > 1 ? this.currentFrame - 2 : -1;
 
     const previousFrame = this.currentFrame > 0 ? this.currentFrame - 1 : -1;
 
+    // strike's bonus calculation for 2nd bonus
     if (secondLastFrame !== -1) {
+      // executes when second last frame is valid
       if (Number(this.knockedPins[secondLastFrame].rolls[0]) === 10) {
         if (Number(this.knockedPins[previousFrame].rolls[0]) === 10) {
           if (this.frameIndex === 0) {
+            // executes when second last frame and last frame has consecutive strikes
+            // then adds the current roll value to the second last frame's cumulative value
             this.knockedPins[secondLastFrame].cumulativeSum = (
               Number(this.knockedPins[secondLastFrame].cumulativeSum) +
               Number(this.knockedPins[this.currentFrame].rolls[0])
             ).toString();
 
+            // and updates the last frame's cumulative value
             this.knockedPins[previousFrame].cumulativeSum = (
               Number(this.knockedPins[previousFrame].cumulativeSum) +
               Number(this.knockedPins[this.currentFrame].rolls[0])
@@ -149,9 +168,13 @@ export class BowlingScoreComponent {
       }
     }
 
+    // strike's bonus calculation for 1st bonus
     if (previousFrame !== -1) {
-      if (+this.knockedPins[previousFrame].rolls[0] === 10) {
+      // executes when last frame is valid
+      if (Number(this.knockedPins[previousFrame].rolls[0]) === 10) {
         if (this.frameIndex === 0) {
+          // executes when last frame has a strike
+          // then adds the first roll value to the last frame's cumulative value
           this.knockedPins[previousFrame].cumulativeSum = (
             Number(this.knockedPins[previousFrame].cumulativeSum) +
             Number(this.knockedPins[this.currentFrame].rolls[0])
@@ -159,6 +182,8 @@ export class BowlingScoreComponent {
         }
 
         if (this.frameIndex === 1) {
+          // executes when last frame has a strike
+          // then adds the second roll value to the last frame's cumulative value
           this.knockedPins[previousFrame].cumulativeSum = (
             Number(this.knockedPins[previousFrame].cumulativeSum) +
             Number(this.knockedPins[this.currentFrame].rolls[1])
@@ -167,7 +192,9 @@ export class BowlingScoreComponent {
       }
     }
 
+    // spare's bonus calculation
     if (previousFrame !== -1) {
+      // executes when last frame is valid
       const previousSpare =
         this.frameIndex === 0 &&
         Number(this.knockedPins[previousFrame].rolls[0]) +
@@ -176,6 +203,8 @@ export class BowlingScoreComponent {
         Number(this.knockedPins[previousFrame].rolls[0]) !== 10;
 
       if (previousSpare) {
+        // executes when a valid spare is found
+        // then adds the first roll value to the last frame's cumulative value
         this.knockedPins[previousFrame].cumulativeSum = (
           Number(this.knockedPins[previousFrame].cumulativeSum) +
           Number(this.knockedPins[this.currentFrame].rolls[0])
@@ -188,6 +217,7 @@ export class BowlingScoreComponent {
         ? this.knockedPins[this.currentFrame - 1].cumulativeSum
         : '';
 
+    // normal cumulative score calculation
     this.knockedPins[this.currentFrame].cumulativeSum =
       this.knockedPins[this.currentFrame].rolls.length === 3
         ? (
@@ -202,14 +232,19 @@ export class BowlingScoreComponent {
             Number(this.knockedPins[this.currentFrame].rolls[1])
           ).toString();
 
+    // total score
     this.totalSum = Number(this.knockedPins[this.currentFrame].cumulativeSum);
   }
 
+  // checks and adds when 10th frame has 3rd index
   private checkThirdRoll(currentScore: number) {
     if (this.frameIndex === 0 && currentScore === 10) {
       if (this.currentFrame === 9) {
+        // adds 3rd index when 10th frame's first roll has a strike
         this.knockedPins[this.currentFrame].rolls.push('');
       } else {
+        // when a strike found, it doesn't execute the second roll
+        // so it goes to next frame
         this.noOfRolls++;
       }
     } else if (
@@ -220,15 +255,20 @@ export class BowlingScoreComponent {
         10 &&
       this.knockedPins[9].rolls.length === 2
     ) {
+      // adds 3rd index when 10th frame's first roll has no strike and a spare found
       this.knockedPins[this.currentFrame].rolls.push('');
     }
   }
 
+  // starts the game, updates the scores and checks whether the game over or not
   private startGame(currentScore: number) {
+    // updates the number of rolls
     this.noOfRolls++;
 
+    // calculates the frame index
     this.frameIndex = this.getRollIndex(this.noOfRolls);
 
+    // calculates current frame from the number of rolls
     this.currentFrame =
       this.currentFrame !== -1 &&
       this.knockedPins[this.currentFrame].rolls.length > 2
@@ -239,12 +279,14 @@ export class BowlingScoreComponent {
       currentScore.toString();
 
     if (this.currentFrame < this.noOfFrames) {
+      // executes upto 10th frame
       this.checkThirdRoll(currentScore);
       this.calculateScore();
       this.checkGameOver();
     }
   }
 
+  // checks available pins for specific knocked pins
   private checkAvailablePins(n: number) {
     if (
       (this.frameIndex === 1 && this.currentFrame !== 9) ||
@@ -253,11 +295,14 @@ export class BowlingScoreComponent {
           Number(this.knockedPins[9].rolls[1]) ===
           10)
     ) {
+      // all pins available, 1) when second roll already executed and it's not 10th frame
+      // 2) when 10th frame has a spare
       this.allPins.map((pins) => {
         pins.isAvailable = true;
         return pins;
       });
     } else {
+      // otherwise, calculates unavailable pins
       const availablePins = n === 10 ? 10 : 10 - n;
 
       for (let i = 0; i <= availablePins; i++) {
